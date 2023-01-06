cb78abccf9e24ff2dd466d60b089ba85cd933f61
MrBoringNumbers <109541909+MrBoringNumbers@users.noreply.github.com>
2023-01-05 19:33:27 -0500

    Add files via upload

--- theme.js
+++ theme.js
@@ -1,4 +1,3 @@
-window.themesDefault = "linear-gradient(45deg, #0087ff, #101671)"
 xhr = new XMLHttpRequest()
 xhr.open('GET', 'themes.json')
 xhr.onload = function() {window.themes = JSON.parse(this.responseText); window.loadThemeButtons()}
